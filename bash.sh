371d98dc88da4b3cf50170d062b26f94c024e4bd
N1ghtedge <lizardasdsad@gmail.com>
2022-10-19 21:29:20 +0300

    first

---
+++ bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+backup_files="~/home/velen/Lab1/"
+dest="~/home/velen/backup/"
+date=$(date +%A)
+hostname=$(hostname -s)
+archive_file="$hostname-$day.tgz"
+echo "Pochinayu rezervne kopiyuvannya $backup_files to $dest/$archive_file."
+
+date
+
+tar czf $dest/$archive_file $backup_files
+echo
+echo "Rezervne kopiyuvannya zaversheno."
+date
